24d7633c50dd69786b5a3045124f286edb2caf05
filip mertens <filip.mertens@ethereum.org>
2024-04-27 07:30:19 +0200

    deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,6 +9,8 @@ git checkout master -- webpack.config.js
 git checkout master -- src/
 yarn && NODE_OPTIONS="--max-old-space-size=4096" yarn build
 cp -R build/* .
+git config user.email "filip.mertens@ethereum.org"
+git config user.name "bunsenstraat"
 git add .
 git commit -m "Built website from {$SHA}."
 git push -f origin gh-pages
